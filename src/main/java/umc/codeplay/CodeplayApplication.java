1166e545ab2072e30fae9ed918c4962f7c46e14c
SeoJimin <jnuni1004@naver.com>
2025-01-07 23:49:53 +0900

    Project Init

---
+++ src/main/java/umc/codeplay/CodeplayApplication.java
@@ -0,0 +1,13 @@
+package umc.codeplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/codeplay/CodeplayApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.codeplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
