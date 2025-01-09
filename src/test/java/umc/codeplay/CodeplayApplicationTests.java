bf2c87797dc2189f8611d2b36c77e92af399a968
Kiara <80676180+2020147542@users.noreply.github.com>
2025-01-09 19:41:07 +0900

    Build ci (#8)
    * build ci script 추가
    
    * spotless 변경사항 자동 git 반영 script 추가
    
    * spotless 자동 실행되도록 gradle 파일 수정
    
    * spotless 설정 수정
    
    * spotless 변경사항 자동 git 반영 script 변경
    
    * env 파일 추가
    
    * env 파일 수정

--- src/test/java/umc/codeplay/CodeplayApplicationTests.java
+++ src/test/java/umc/codeplay/CodeplayApplicationTests.java
@@ -2,12 +2,14 @@ package umc.codeplay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
+@ComponentScan(basePackages = "umc.codeplay")
 class CodeplayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
