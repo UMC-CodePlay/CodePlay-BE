aac9f78541be2f0204f05c03cb3883367ed7b617
Kiara <80676180+2020147542@users.noreply.github.com>
2025-01-10 18:59:06 +0900

    #15 ci test (#17)
    * spotless 설정 변경됨
    
    * build ci - spotless 부분 수정
    
    * 안되게 test
    
    * 원복

--- src/test/java/umc/codeplay/CodeplayApplicationTests.java
+++ src/test/java/umc/codeplay/CodeplayApplicationTests.java
@@ -1,15 +1,16 @@
 package umc.codeplay;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
+
+import org.junit.jupiter.api.Test;
 
 @ActiveProfiles("test")
 @SpringBootTest
 @ComponentScan(basePackages = "umc.codeplay")
 class CodeplayApplicationTests {
 
-    @Test
-    void contextLoads() {}
+  @Test
+  void contextLoads() {}
 }
