96d9d6232b7cebc0fb884d3fdd9bab46be0bc647
Kiara <80676180+2020147542@users.noreply.github.com>
2025-01-19 03:08:22 +0900

    [Feat] client 요청시 cors 전체 허용으로 설정 (#59)

---
+++ src/main/java/umc/codeplay/config/WebConfig.java
@@ -0,0 +1,23 @@
+package umc.codeplay.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final String[] ALLOWED_METHOD_NAMES = {
+        "GET", "HEAD", "POST", "PUT", "DELETE", "TRACE", "OPTIONS", "PATCH"
+    };
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해서 cors 허용
+                .allowedOrigins("*") // 모든 출처로 부터 요청 허용 - TODO: 변경해야 함
+                .allowedMethods(ALLOWED_METHOD_NAMES)
+                .allowedHeaders("Content-Type", "Authorization")
+                .maxAge(3600); // 캐싱 시간 설정
+        // .exposedHeaders("Authorization") // 클라이언트가 접근할 수 있는 헤더
+    }
+}
