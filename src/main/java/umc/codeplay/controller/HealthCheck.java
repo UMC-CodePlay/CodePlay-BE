6280b67cf97d55b6004c60275483940369a976a6
2020147542 <tmfal5023@yonsei.ac.kr>
2025-01-19 02:30:02 +0900

    [fix] responseEntity 사용

--- src/main/java/umc/codeplay/controller/HealthCheck.java
+++ src/main/java/umc/codeplay/controller/HealthCheck.java
@@ -1,5 +1,6 @@
 package umc.codeplay.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheck {
 
     @GetMapping("/health")
-    public String healthCheck() {
-        return "OK";
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("UMC 7th CodePlay Well Connected!");
     }
 }
