a4124384e24e0f31777a14f0c795b04f1880e068
2020147542 <tmfal5023@yonsei.ac.kr>
2025-01-26 19:35:40 +0900

    [Test] 필요없는 부분 제거 테스트

--- src/main/java/umc/codeplay/config/AWSConfig.java
+++ src/main/java/umc/codeplay/config/AWSConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -22,16 +21,16 @@ public class AWSConfig {
     @Value("${spring.cloud.aws.region.static}")
     private String region;
 
-    @Bean
-    public S3Client s3Client() {
-        System.out.println("s3Client region: " + region);
-        return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey, secretKey)))
-                .build();
-    }
+    //    @Bean
+    //    public S3Client s3Client() {
+    //        System.out.println("s3Client region: " + region);
+    //        return S3Client.builder()
+    //                .region(Region.of(region))
+    //                .credentialsProvider(
+    //                        StaticCredentialsProvider.create(
+    //                                AwsBasicCredentials.create(accessKey, secretKey)))
+    //                .build();
+    //    }
 
     @Bean
     public S3Presigner s3Presigner() {
