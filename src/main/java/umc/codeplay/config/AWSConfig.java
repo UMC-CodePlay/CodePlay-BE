c695292820210cfe15fa9f98fdb1dd43fd8eed27
Kiara <80676180+2020147542@users.noreply.github.com>
2025-01-26 19:41:28 +0900

    [Test] 필요없는 부분 제거 테스트 (#86)
    * [Test] 필요없는 부분 제거 테스트
    
    * [Chore] 필요없는 코드 제거

--- src/main/java/umc/codeplay/config/AWSConfig.java
+++ src/main/java/umc/codeplay/config/AWSConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -23,19 +22,7 @@ public class AWSConfig {
     private String region;
 
     @Bean
-    public S3Client s3Client() {
-        System.out.println("s3Client region: " + region);
-        return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey, secretKey)))
-                .build();
-    }
-
-    @Bean
     public S3Presigner s3Presigner() {
-        System.out.println("s3Presigner region: " + region);
         return S3Presigner.builder()
                 .region(Region.of(region))
                 .credentialsProvider(
