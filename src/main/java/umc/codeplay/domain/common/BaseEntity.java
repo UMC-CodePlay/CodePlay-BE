5eac6168852d60ad15d3ad56c31c5d28a11730bf
2020147542 <tmfal5023@yonsei.ac.kr>
2025-01-26 16:57:07 +0900

    [Fix] baseEntity 자동 반영되도록 수정

--- src/main/java/umc/codeplay/domain/common/BaseEntity.java
+++ src/main/java/umc/codeplay/domain/common/BaseEntity.java
@@ -14,6 +14,7 @@ import lombok.Getter;
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 public abstract class BaseEntity {
+
     @CreatedDate private LocalDateTime createdAt;
 
     @LastModifiedDate private LocalDateTime updatedAt;
