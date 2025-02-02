dfde7ffa2027eed660aa628a9516b04ea2c36eee
Kiara <80676180+2020147542@users.noreply.github.com>
2025-02-02 14:23:34 +0900

    [Refactor] fileUpload시 image일 경우 분기 (#102)

--- src/main/java/umc/codeplay/dto/FileResponseDTO.java
+++ src/main/java/umc/codeplay/dto/FileResponseDTO.java
@@ -7,14 +7,9 @@ public class FileResponseDTO {
 
     @Getter
     @AllArgsConstructor
-    public static class DownloadFile {
-        private String downloadS3Url;
-    }
-
-    @Getter
-    @AllArgsConstructor
     public static class UploadFile {
         private String uploadS3Url;
         private Long musicId;
+        private Long memberId;
     }
 }
--- src/main/java/umc/codeplay/service/FileService.java
+++ src/main/java/umc/codeplay/service/FileService.java
@@ -8,11 +8,8 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
-import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 import umc.codeplay.apiPayLoad.code.status.ErrorStatus;
@@ -48,31 +45,6 @@ public class FileService {
         return normalizedFileName.replaceAll("\\s+", "_").replaceAll("[^가-힣a-zA-Z0-9.\\-_]", "_");
     }
 
-    // S3에서 파일을 다운로드할 수 있는 Presigned URL 생성
-    public String generateGetPresignedUrl(Long musicId) {
-        try {
-            Music music =
-                    musicRepository
-                            .findById(musicId)
-                            .orElseThrow(() -> new GeneralHandler(ErrorStatus.MUSIC_NOT_FOUND));
-
-            GetObjectRequest getObjectRequest =
-                    GetObjectRequest.builder().bucket(bucketName).key(music.getTitle()).build();
-
-            GetObjectPresignRequest presignRequest =
-                    GetObjectPresignRequest.builder()
-                            .signatureDuration(Duration.ofMinutes(60))
-                            .getObjectRequest(getObjectRequest)
-                            .build();
-
-            PresignedGetObjectRequest presignedRequest =
-                    s3Presigner.presignGetObject(presignRequest);
-            return presignedRequest.url().toString();
-        } catch (Exception e) {
-            throw new GeneralHandler(ErrorStatus.AWS_SERVICE_UNAVAILABLE);
-        }
-    }
-
     // S3에 파일을 업로드할 수 있는 Presigned URL 생성
     public String generatePutPresignedUrl(String fileName) {
         try {
@@ -93,6 +65,20 @@ public class FileService {
         }
     }
 
+    // User 레포지토리에 업로드
+    public Long uploadProfile(String newFileName, String userEmail) {
+        Member member =
+                memberRepository
+                        .findByEmail(userEmail)
+                        .orElseThrow(() -> new GeneralHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+        String s3Url =
+                String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, newFileName);
+
+        member.setProfileUrl(s3Url);
+        return memberRepository.save(member).getId();
+    }
+
     // music 레포지토리에 업로드
     public Long uploadMusic(String newFileName, String userEmail) {
         Member member =
@@ -101,13 +87,10 @@ public class FileService {
                         .orElseThrow(() -> new GeneralHandler(ErrorStatus.MEMBER_NOT_FOUND));
 
         // 저장하는 url은 유효시간이 없는 public
-        // TODO: 업로드에만 presigned 사용할지 아님 다운로드시에도 사용할지에 따라 변경해야함.
         String s3Url =
                 String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, newFileName);
         Music newMusic = Music.builder().title(newFileName).musicUrl(s3Url).member(member).build();
 
         return musicRepository.save(newMusic).getId();
     }
-
-    // TODO: 필요시 직접 업로드 방법 구현 필요
 }
