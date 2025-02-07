c53c7179fba2a40303585f42ef3a59a1a4f7774c
SeoJimin <jnuni1004@naver.com>
2025-02-07 23:14:20 +0900

    feat/MypageSearch/#33

--- src/main/java/umc/codeplay/repository/MusicRepository.java
+++ src/main/java/umc/codeplay/repository/MusicRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import umc.codeplay.domain.Music;
 
-public interface MusicRepository extends JpaRepository<Music, Long> {}
+public interface MusicRepository extends JpaRepository<Music, Long> {
+    // 제목으로 음원 찾기
+    Music findByTitle(String title);
+}
--- src/main/java/umc/codeplay/repository/TrackRepository.java
+++ src/main/java/umc/codeplay/repository/TrackRepository.java
@@ -3,11 +3,18 @@ package umc.codeplay.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import umc.codeplay.domain.Member;
+import umc.codeplay.domain.Music;
 import umc.codeplay.domain.Track;
 
 public interface TrackRepository extends JpaRepository<Track, Long> {
     // 특정 사용자의 Track 리스트 조회
     List<Track> findByMusicMember(Member member);
+
+    // 특정 사용자의 Harmony 리스트 중 music의 harmony 조회
+    @Query("SELECT t FROM Track t " + "WHERE t.music = :music AND t.music.member = :member")
+    List<Track> findByMusicAndMember(@Param("member") Member member, @Param("music") Music music);
 }
