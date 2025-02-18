e47f20f404c303e47a7a3297e632bcd4d94e92af
2020147542 <tmfal5023@yonsei.ac.kr>
2025-02-19 00:42:37 +0900

    Style: 주석처리

--- src/main/java/umc/codeplay/controller/TaskController.java
+++ src/main/java/umc/codeplay/controller/TaskController.java
@@ -1,16 +1,11 @@
 package umc.codeplay.controller;
 
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import umc.codeplay.apiPayLoad.ApiResponse;
@@ -75,6 +70,7 @@ public class TaskController {
         return ApiResponse.onSuccess(MemberConverter.toTaskProgressDTO(task));
     }
 
+    @Hidden // TODO: 기능 완성시 @Hidden 태그만 삭제해주세요!
     @Operation(
             summary = "작업 진행 상황 조회",
             description = "작업 ID를 받아 완료될 때까지 기다린 후 결과를 반환합니다. 기본 대기시간 5분, 10초마다 작업 상태확인.")
